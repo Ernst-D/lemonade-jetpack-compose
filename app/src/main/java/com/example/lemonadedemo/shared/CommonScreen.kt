48cf641dda42881676d4362fc086775bec8199eb
Ernst-D <ernst.dzerevanko@gmail.com>
2024-02-05 13:34:08 +0400

    colors and header

--- app/src/main/java/com/example/lemonadedemo/shared/CommonScreen.kt
+++ app/src/main/java/com/example/lemonadedemo/shared/CommonScreen.kt
@@ -1,26 +1,24 @@
 package com.example.lemonadedemo.shared
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonadedemo.R
+import com.example.lemonadedemo.ui.theme.LemonadeDemoTheme
+import com.example.lemonadedemo.ui.theme.Yellow
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CommonScreen(image: String, textContent: String) {
-    Scaffold(topBar = {
+    Scaffold(
+        topBar = {
         Header()
     }) {
         Column {
@@ -31,16 +29,13 @@ fun CommonScreen(image: String, textContent: String) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun Header() {
-    val customColor = Yellow
-
+private fun Header(modifier: Modifier = Modifier) {
     // TopAppBar with custom background color
     TopAppBar(
-        modifier = Modifier
-            .background(customColor),
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Yellow),
         title = {
             Text(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
                 text = stringResource(id = R.string.header_text),
                 fontWeight = FontWeight.Bold
@@ -61,5 +56,7 @@ private fun HeaderPreview() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CommonScreenPreview() {
-    CommonScreen(image = "test string", textContent = "test content")
+    LemonadeDemoTheme {
+        CommonScreen(image = "test string", textContent = "test content")
+    }
 }
