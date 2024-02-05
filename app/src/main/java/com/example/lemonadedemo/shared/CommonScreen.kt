aa46e5e93ffc108673c9f8fde81673bb93eebca9
Ernst-D <ernst.dzerevanko@gmail.com>
2024-02-05 12:39:50 +0400

    more on preview

--- app/src/main/java/com/example/lemonadedemo/shared/CommonScreen.kt
+++ app/src/main/java/com/example/lemonadedemo/shared/CommonScreen.kt
@@ -1,39 +1,50 @@
 package com.example.lemonadedemo.shared
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Yellow
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.lemonadedemo.R
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun CommonScreen(image: String, textContent: String){
+fun CommonScreen(image: String, textContent: String) {
+    Scaffold(topBar = {
+        Header()
+    }) {
+        Column {
 
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Header() {
-    val customColor = Color.Yellow
+    val customColor = Yellow
 
     // TopAppBar with custom background color
     TopAppBar(
         modifier = Modifier
             .background(customColor),
         title = {
-            Text(text = "Your App Title")
-        },
-        navigationIcon = {
-            IconButton(onClick = { /* Handle navigation icon click */ }) {
-                Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
-            }
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = stringResource(id = R.string.header_text),
+                fontWeight = FontWeight.Bold
+            )
         },
         actions = {
             // You can add any actions here
@@ -41,14 +52,14 @@ private fun Header() {
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-private fun HeaderPreview(){
+private fun HeaderPreview() {
     Header()
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun CommonScreenPreview(){
-
+fun CommonScreenPreview() {
+    CommonScreen(image = "test string", textContent = "test content")
 }
