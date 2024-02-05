914ac6321c94888689b7f4c0fafd18b93a757708
Ernst-D <ernst.dzerevanko@gmail.com>
2024-02-05 21:12:20 +0400

    correct logic

--- app/src/main/java/com/example/lemonadedemo/core/LemonadeBox.kt
+++ app/src/main/java/com/example/lemonadedemo/core/LemonadeBox.kt
@@ -5,12 +5,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -22,10 +28,31 @@ import com.example.lemonadedemo.R
 @Composable
 fun LemonadeBox(
     modifier: Modifier,
-    image: Painter,
-    contentDesc: String
 ) {
     val context = LocalContext.current
+    var currentScreen by remember {
+        mutableStateOf(0)
+    }
+
+    var squeezeNumber by remember {
+        mutableStateOf(0)
+    }
+
+    val getImage = when (currentScreen) {
+        0 -> R.drawable.lemon_tree
+        1 -> R.drawable.lemon_squeeze
+        2 -> R.drawable.lemon_drink
+        3 -> R.drawable.lemon_restart
+        else -> R.drawable.lemon_tree
+    }
+
+    val getContentDesc = when (currentScreen) {
+        0 -> R.string.lemon_tree_text
+        1 -> R.string.keep_tapping_content_desc
+        2 -> R.string.lemonade_to_drink_content_desc
+        3 -> R.string.keep_tapping_content_desc
+        else -> R.string.lemon_tree_text
+    }
 
     Box(
         contentAlignment = Alignment.Center,
@@ -36,23 +63,73 @@ fun LemonadeBox(
             )
             .size(250.dp, 250.dp)
             .clickable {
+                currentScreen++
                 Toast
-                    .makeText(context, "Button Clicked!", Toast.LENGTH_SHORT)
+                    .makeText(context, "Button Clicked! Screen $currentScreen", Toast.LENGTH_SHORT)
                     .show()
             }
     ) {
-        Image(painter = image, contentDescription = contentDesc)
+        Image(
+            painter = painterResource(id = getImage),
+            contentDescription = stringResource(id = getContentDesc)
+        )
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
 fun LemonadeBoxPreview() {
     LemonadeBox(
-        modifier = Modifier,
-        image = painterResource(id = R.drawable.lemon_squeeze),
-        contentDesc = stringResource(
-            id = R.string.keep_tapping_text
-        )
+        modifier = Modifier
     )
+}
+
+@Composable
+fun LemonadeBoxPattern(screenNumber: Int): @Composable () -> Unit {
+    return when (screenNumber) {
+        0 -> {
+            {
+                LemonadeBox(
+                    modifier = Modifier,
+
+                    )
+            }
+        }
+
+        1 -> {
+            {
+                LemonadeBox(
+                    modifier = Modifier,
+                )
+            }
+        }
+
+        else -> {
+            { Text(text = "else block") }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun LemonadeBoxPatternPreview() {
+    var currentScreen by remember {
+        mutableStateOf(0)
+    }
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier
+        .background(
+            Color.Green,
+            shape = RoundedCornerShape(16.dp)
+        )
+        .size(300.dp, 300.dp)
+        .fillMaxSize()
+        .clickable {
+            currentScreen++
+            if (currentScreen > 2) currentScreen = 0
+        }) {
+        val lemonadeComposable = LemonadeBoxPattern(screenNumber = currentScreen)
+        lemonadeComposable()
+    }
 }
